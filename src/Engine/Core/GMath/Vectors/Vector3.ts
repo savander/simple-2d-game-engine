e71cea5c9c97179f191bf1e6778c18db11043b26
Savander <savander.pl@gmail.com>
2018-10-24 17:43:25 +0200

    Vector3 now offers Vector3.zero attribute.

--- src/Engine/Core/GMath/Vectors/Vector3.ts
+++ src/Engine/Core/GMath/Vectors/Vector3.ts
@@ -1,9 +1,12 @@
 export class Vector3 {
+
+    static zero: Vector3 = new Vector3(0, 0, 0);
+
     x: number;
     y: number;
     z: number;
 
-    constructor(x: number, y = 0, z = 0) {
+    constructor(x: number = 0, y: number = 0, z: number = 0) {
         this.x = x;
         this.y = y;
         this.z = z;
